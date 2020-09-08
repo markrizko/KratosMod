e74ea26ad7755d2e159fe370e6526d389f971d74
Mark Rizko <mark@rizkocircle.com>
2020-09-07 23:58:37 -0700

    Initial upload, blades of chaos card and ablockalypse created and have effects, rest is from theDefault

---
+++ src/main/java/SatanParlor/cards/ablockalypse.java
@@ -0,0 +1,75 @@
+package SatanParlor.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import SatanParlor.DefaultMod;
+import SatanParlor.characters.TheDefault;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+import static SatanParlor.DefaultMod.makeCardPath;
+
+public class ablockalypse extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = DefaultMod.makeID(ablockalypse.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("ablockalypse.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.RARE; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.SELF;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;       //
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 5;
+    private static final int UPGRADED_COST = 7;
+
+    private static final int TOTAL = 60;
+    private static final int UPGRADE_PLUS_TOTAL = 40;
+
+
+    // /STAT DECLARATION/
+
+
+    public ablockalypse() { // public ablockalypse() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        //baseBUFF/BLOCK = 60;
+        baseBlock = TOTAL;
+        this.exhaust = true;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p,p,block));
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p,p, new VulnerablePower(p,5, false), 5));
+                // action here
+    }
+
+
+    // Upgraded stats.
+    // DON'T FORGET TO ADD UPGRADE BLOCK OR BUFF HERE
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADED_COST);
+            upgradeBlock(UPGRADE_PLUS_TOTAL);
+            initializeDescription();
+        }
+    }
+}
---
+++ src/main/java/SatanParlor/cards/blades.java
@@ -0,0 +1,85 @@
+package SatanParlor.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import SatanParlor.DefaultMod;
+import SatanParlor.characters.TheDefault;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import java.util.Iterator;
+
+import static SatanParlor.DefaultMod.makeCardPath;
+
+// public class blades extends AbstractDynamicCard
+public class blades extends AbstractDynamicCard {
+
+
+    // TEXT DECLARATION
+
+    public static final String ID = DefaultMod.makeID(blades.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("blades.png");// "public static final String IMG = makeCardPath("blades.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 4;
+    private static final int UPGRADED_COST = 4;
+
+    private static final int DAMAGE = 50;
+    private static final int UPGRADE_PLUS_DMG = 25;
+
+    // /STAT DECLARATION/
+
+
+    public blades() { // public blades() - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        this.baseMagicNumber = 5;
+        this.magicNumber = this.baseMagicNumber;
+        this.exhaust = true;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+
+        AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, this.multiDamage, this.damageTypeForTurn, AbstractGameAction.AttackEffect.FIRE));
+
+        Iterator var3 = AbstractDungeon.getCurrRoom().monsters.monsters.iterator();
+
+        while(var3.hasNext()) {
+            AbstractMonster mo = (AbstractMonster)var3.next();
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new WeakPower(mo, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(mo, p, new VulnerablePower(mo, this.magicNumber, false), this.magicNumber, true, AbstractGameAction.AttackEffect.NONE));
+        }
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
