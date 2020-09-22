bc3e9133a69040eb73a3a6c3a9e05dc607d92944
Mark Rizko <mark@rizkocircle.com>
2020-09-22 16:30:29 -0700

    Evade power created, very useful. Roll card also created demostrates how to use Evade.

--- src/main/java/SatanParlor/DefaultMod.java
+++ src/main/java/SatanParlor/DefaultMod.java
@@ -441,6 +441,7 @@ public class DefaultMod implements
         BaseMod.addCard(new settle());
         BaseMod.addCard(new kBerserk());
         BaseMod.addCard(new FullCounter());
+        BaseMod.addCard(new Roll());
         
         logger.info("Making sure the cards are unlocked.");
         // Unlock the cards
@@ -469,6 +470,7 @@ public class DefaultMod implements
         UnlockTracker.unlockCard(settle.ID);
         UnlockTracker.unlockCard(kBerserk.ID);
         UnlockTracker.unlockCard(FullCounter.ID);
+        UnlockTracker.unlockCard(Roll.ID);
 
         logger.info("Done adding cards!");
     }
